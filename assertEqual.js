b2a0ff35010a61d85749d622d70b2984c173f32a
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-09-15 02:52:59 +0000

    created a assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  const passed = String.fromCodePoint(0x1F929);
+  const failed = String.fromCodePoint(0x1F62C);
+
+  if (actual === expected) {
+    console.log(passed + passed + passed + " Assertion Passed! " + actual + " === " + expected);
+  } else {
+    console.log(failed + failed + failed + " Assertion Failed: " + actual + " !== " + expected);
+  }
+return 0
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
