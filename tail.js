5b22ed5ad02ca76d41d9b1b9f904c1de21147886
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-09-15 04:07:52 +0000

    Created a function to get the tail end of an array

---
+++ tail.js
@@ -0,0 +1,24 @@
+//Implementing Tail function
+const tail = function(list) {
+  return list.slice(1);
+};
+
+console.log(tail([]));
+
+
+//TESTING THE CODE
+const assertEqual = function(actual, expected) {
+  const passed = String.fromCodePoint(0x1F929);
+  const failed = String.fromCodePoint(0x1F62C);
+
+  if (actual === expected) {
+    console.log(`${passed} ${passed} ${passed} Assertion Passed! ${actual} === ${expected}`);
+  } else {
+    console.log(`${failed} ${failed} ${failed} Assertion Failed: ${actual} !== ${expected}`);
+  }
+  return 0;
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
