9d014e06839fd0998265a0ea3821846059ae75bc
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-09-15 03:15:12 +0000

    creating a head function that outputs the first element of the array

---
+++ head.js
@@ -0,0 +1,25 @@
+// Implement Head funciton 
+const head = function(list) {
+  return list[0];
+};
+
+
+// TEST CODE with assertEqual function 
+const assertEqual = function(actual, expected) {
+  const passed = String.fromCodePoint(0x1F929);
+  const failed = String.fromCodePoint(0x1F62C);
+
+  if (actual === expected) {
+    console.log(`${passed} ${passed} ${passed} Assertion Passed! ${actual} === ${expected}`);
+  } else {
+    console.log(`${failed} ${failed} ${failed} Assertion Failed: ${actual} !== ${expected}`);
+  }
+return 0
+};
+
+//assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual(1, 1);
+assertEqual(head([5,67]), 5); 
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+
