d6dccb4218d61e8e908799ecf6a33cef834169e3
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-09-21 22:18:54 +0000

    Created a eqArrays test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,16 @@
-const assertEqual = function(actual, expected) {
-  const passed = String.fromCodePoint(0x1F929);
-  const failed = String.fromCodePoint(0x1F62C);
+// const assertEqual = function(actual, expected) {
+//   const passed = String.fromCodePoint(0x1F929);
+//   const failed = String.fromCodePoint(0x1F62C);
 
-  if (actual === expected) {
-    console.log(`${passed} ${passed} ${passed} Assertion Passed! ${actual} === ${expected}`);
-  } else {
-    console.log(`${failed} ${failed} ${failed} Assertion Failed: ${actual} !== ${expected}`);
-  }
-return 0
-};
+//   if (actual === expected) {
+//     console.log(`${passed} ${passed} ${passed} Assertion Passed! ${actual} === ${expected}`);
+//   } else {
+//     console.log(`${failed} ${failed} ${failed} Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// return 0
+// };
+
+//const assertEqual = require('./assertEqual');
 
 let eqArrays = function (arrayOne, arrayTwo) {
   for (let i=0; i < arrayOne.length; i++){
@@ -22,15 +24,15 @@ let eqArrays = function (arrayOne, arrayTwo) {
   return eqArrays
 }
 
-
+module.exports = eqArrays
 //console.log(eqArrays([1,2,3], [1,2,3])); 
 //console.log (eqArrays ([1, 2, 3], [3, 2, 1]));
 
 //console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
 //console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 3]), true);
 //assertEqual(eqArrays([“No thank you”], [“Yes Please”]), false);
 //assertEqual(eqArrays([“Hello”], [“Hello”]), true);
 
