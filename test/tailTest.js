f7c07636a63092d0903c776d307052fc7b9511db
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-09-22 00:23:51 +0000

    Refractored tailTest with Mocha functions and Chai assertion library

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,16 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+
+const assert = require ('chai').assert;
 const tail = require('../tail')
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-//console.log(tail(words));
-assertEqual(tail(words), ["Lighthouse", "Labs"]);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// //console.log(tail(words));
+// assertEqual(tail(words), ["Lighthouse", "Labs"]);
+
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+});
+
+
